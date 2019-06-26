0a44f6ec3f59d1478883fcdda2f5e4d5e9a02707
zhengyu14 <jan25th@live.cn>
2019-06-26 16:34:23 +0800

    Add Tinysoft component

--- DataProvider.py
+++ DataProvider.py
@@ -1,27 +1,20 @@
 import jqdatasdk as jqd
+import TSLPy3 as tsl
 import ConstantLib as cl
 import pandas as pd
 
 class dataProvider:
-    # Attributes
-    securityIDCode = cl.securityIDCode000001XSHE # default as 000001.XSHE
-    securityData = pd.DataFrame()
-    log = []
-
-    # Initialize instance and log-in JQData
+    # Initialize instance
     def __init__(self):
-        # Authenticated when 'auth success' is shown
-        jqd.auth(cl.jqDataID, cl.jqDataPassword)
-
-    # Log-out JQData when max. connection (3) is reached
-    def logout(self):
-        jqd.logout()
-
-    # Log a new message
-    def add_log(self, message):
-        if message != '':
-            self.log.append(message)
-    
+        # Initialize attributes
+        self.securityIDCode = cl.securityIDCode000001XSHE # default as 000001.XSHE
+        self.securityData = pd.DataFrame()
+        self.alpha = pd.DataFrame(columns = [cl.tinysoftAlphaCols])
+        self.log = []
+        
+    '''
+    Public functions
+    '''
     # Get log
     def get_log(self):
         return self.log
@@ -32,13 +25,63 @@ class dataProvider:
         if securityID is not None and securityCode is not None:
             self.securityIDCode = securityID + '.' + securityCode
     
-    # Get security daily data
+    # Get security data
     #   field: 'None' means default ['open', 'close', 'high', 'low', 'volume', 'money']
     #   skip_paused: 'True' means skipping none-trading date (停牌, 未上市或者退市后)
     #   fq: 复权选项: 'pre': 前复权; None: 不复权, 返回实际价格; 'post': 后复权
-    def get_security_data_min(self, startDatetime, endDatetime):
+    def get_security_data_daily(self, startDate, endDate):
+        self._login_jqdata()
         try:
-            self.securityData = jqd.get_price(self.securityIDCode, start_date=startDatetime, end_date=endDatetime, frequency='minute', fields=None, skip_paused=True, fq='pre')
+            self.securityData = jqd.get_price(self.securityIDCode, start_date=startDate, end_date=endDate, frequency=cl.jqDataFreqDaily, fields=None, skip_paused=True, fq='pre')
         except:
-            self.add_log(cl.msgInvalidSecurityIDCode)
-        return self.securityData    
+            self._add_log(cl.msgInvalidSecurityIDCode)
+        self._logout_jqdata()
+        return self.securityData
+    
+    def get_security_data_min(self, startDatetime, endDatetime):
+        self._login_jqdata()
+        try:
+            self.securityData = jqd.get_price(self.securityIDCode, start_date=startDatetime, end_date=endDatetime, frequency=cl.jqDataFreqDaily, fields=None, skip_paused=True, fq='pre')
+        except:
+            self._add_log(cl.msgInvalidSecurityIDCode)
+        self._logout_jqdata()
+        return self.securityData
+
+    # Get alpha data
+    def get_alpha(self):
+        self._login_tinysoft()
+        alphaData3 = tsl.Get101alphaByID(3)
+        self._logout_tinysoft()
+    
+    '''
+    Private functions
+    '''
+    # Log-in JQData
+    def _login_jqdata(self):
+        # Authenticated when 'auth success' is shown
+        jqd.auth(cl.jqDataID, cl.jqDataPassword)
+
+    # Log-out JQData
+    def _logout_jqdata(self):
+        jqd.logout()
+
+    # Log-in Tinysoft
+    def _login_tinysoft(self):
+        tsl.ConnectServer(cl.tinysoftServer, cl.tinysoftPort)
+        dl = tsl.LoginServer(cl.tinysoftID, cl.tinysoftPassword)
+        if dl[0] == 0:
+            tsl.SetComputeBitsOption(64)
+        try:
+            test = tsl.RemoteExecute("return 1;", {})
+            test[1]+1
+        except:
+            self._add_log(cl.msgLoginTinysoftFailed)
+
+    # Log-out Tinysoft
+    def _logout_tinysoft(self):
+        tsl.Disconnect()
+
+    # Log a new message
+    def _add_log(self, message):
+        if message != '':
+            self.log.append(message)
