2c8a8251565a25f4496bb78bc5b51c7fe8e76876
ZhoumiaoShi <zs1277@nyu.edu>
2019-07-01 13:25:11 +0800

    szm_20190701

--- ConstantLib.py
+++ ConstantLib.py
@@ -1,17 +1,41 @@
 # Constants
 
+import jqdatasdk as jqd
+
 '''
 JQData platform
 '''
 jqDataID = '18068008158'
 jqDataPassword = 'Yu19920125'
+jqDataFreqDaily = 'daily'
+jqDataFreqMin = 'minute'
+
+securityIDCode000001XSHE = '000001.XSHE'
 
 '''
-Security ID
+Tinysoft platform
 '''
-securityIDCode000001XSHE = '000001.XSHE'
+tinysoftServer = 'tsl.tinysoft.com.cn'
+tinysoftPort = 443
+tinysoftID = 'axzq'
+tinysoftPassword = 'ax888888'
+
+tinysoftAlphaCols = ['Alpha003','Alpha004','Alpha005','Alpha006','Alpha008', 
+                    'Alpha011','Alpha012','Alpha013','Alpha014','Alpha015', 
+                    'Alpha018','Alpha019','Alpha021','Alpha022','Alpha023',
+                    'Alpha024','Alpha025','Alpha026','Alpha028','Alpha030']
 
 '''
 Message text
 '''
 msgInvalidSecurityIDCode = 'Invalid security ID or code.'
+<<<<<<< HEAD
+
+'''
+Factor name
+'''
+factorPbRatio = jqd.valuation.pb_ratio
+factorCirculatingMarketCap = jqd.valuation.circulating_market_cap
+=======
+msgLoginTinysoftFailed = 'Failed to log-in Tinysoft.'
+>>>>>>> 0a44f6ec3f59d1478883fcdda2f5e4d5e9a02707
