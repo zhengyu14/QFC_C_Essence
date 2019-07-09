b1c6cd2af11686ea3528d014ee8f11fede65c7dc
Zhengyu <jan25th@live.cn>
2019-07-09 17:08:34 +0800

    Update _visualizer.py

--- _data/_visualizer.py
+++ _data/_visualizer.py
@@ -8,7 +8,7 @@ import _painter as pt
 painter = pt.painter(1,25)
 
 # Show figures of one specific alpha factor
-painter.show_factor_single(2)
+painter.show_factor_single(14)
 
 # Show and save figures within interval (it may take several minutes processing the data)
 #painter.show_factor_all()
